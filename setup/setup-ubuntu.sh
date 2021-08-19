4ffbcd44971b5eddf60f240e290bb6569cacaeb8
Noah Lloyd <63750956+NoahLloyd@users.noreply.github.com>
2021-08-19 23:25:38 +0200

    Create setup-ubuntu.sh

---
+++ setup/setup-ubuntu.sh
@@ -0,0 +1,17 @@
+# Update and install snap
+apt update
+apt install snapd
+# Install apps
+snap install discord-canary
+snap install --classic code
+snap install brave
+# Edit .vimrc
+cd $HOME
+# Edit .bashrc
+echo 'export PS1="\[\033[01;32m\]\@\[\033[00m\] | \[\033[01;34m\]\W\[\033[00m\] $ "' >> .bashrc
+# Map keys
+apt-get install x11-xkb-utils
+setxkbmap -option caps:backspace
+setxkbmap -option backspace:caps
+setxkbmap -option tab:swapescape
+setxkbmap -option swapescape:tab
